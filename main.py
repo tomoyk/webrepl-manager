f2552a591baf3cf2c1dabedeaf182443d33ce8d3
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2022-01-05 21:09:53 +0900

    update code

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import webrepl
 
-config = [
-    {
-        'host': '192.168.0.7',
-        'port': 8266,
-        'password': 'pass'
-    }
-]
 
-
-def main():
-    repl = webrepl.Webrepl(
-        **(config[0]))
+def exec(**args):
+    print(args)
+    repl = webrepl.Webrepl(**args)
     resp = repl.sendcmd(
         "import network; sta_if = network.WLAN(network.STA_IF); sta_if.ifconfig();")
     print(resp.decode("ascii"))
+
+
+def main():
+    configs = [
+        {
+            'host': '192.168.0.17',
+            'port': 8266,
+            'password': 'pass'
+        },
+        {
+            'host': '192.168.0.18',
+            'port': 8266,
+            'password': 'pass'
+        }
+    ]
+    for conf in configs:
+        exec(**conf)
 
 
 if __name__ == "__main__":
